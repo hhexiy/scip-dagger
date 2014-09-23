635a12f9715ad87c03560665cfe11e4328ac655b
heherebecca <hehe.rebecca@gmail.com>
2014-09-23 10:26:17 -0400

    first commit

---
+++ src/cmain.c
@@ -0,0 +1,332 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2014 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**@file   cmain.c
+ * @brief  Main file for binpacking pricing example
+ * @author Timo Berthold
+ * @author Stefan Heinz
+ *
+ *  This the file contains the \ref main() main function of the projects. This includes all the default plugins of
+ *  \SCIP and the once which belong to that projects. After that is starts the interactive shell of \SCIP or processes
+ *  the shell arguments if given.
+ */
+#include <stdio.h>
+#include <string.h>
+
+#include "scip/scip.h"
+#include "scip/scipshell.h"
+#include "scip/scipdefplugins.h"
+
+#include "nodesel_oracle.h"
+
+static
+SCIP_RETCODE fromCommandLine(
+   SCIP*                 scip,               /**< SCIP data structure */
+   const char*           filename            /**< input file name */
+   )
+{
+   SCIP_RETCODE retcode;
+
+   /********************
+    * Problem Creation *
+    ********************/
+
+   /** @note The message handler should be only fed line by line such the message has the chance to add string in front
+    *        of each message
+    */
+   SCIPinfoMessage(scip, NULL, "\n");
+   SCIPinfoMessage(scip, NULL, "read problem <%s>\n", filename);
+   SCIPinfoMessage(scip, NULL, "============\n");
+   SCIPinfoMessage(scip, NULL, "\n");
+
+
+   retcode = SCIPreadProb(scip, filename, NULL);
+
+   switch( retcode )
+   {
+   case SCIP_NOFILE:
+      SCIPinfoMessage(scip, NULL, "file <%s> not found\n", filename);
+      return SCIP_OKAY;
+   case SCIP_PLUGINNOTFOUND:
+      SCIPinfoMessage(scip, NULL, "no reader for input file <%s> available\n", filename);
+      return SCIP_OKAY;
+   case SCIP_READERROR:
+      SCIPinfoMessage(scip, NULL, "error reading file <%s>\n", filename);
+      return SCIP_OKAY;
+   default:
+      SCIP_CALL( retcode );
+   } /*lint !e788*/
+
+   /*******************
+    * Problem Solving *
+    *******************/
+
+   /* solve problem */
+   SCIPinfoMessage(scip, NULL, "\nsolve problem\n");
+   SCIPinfoMessage(scip, NULL, "=============\n\n");
+
+   SCIP_CALL( SCIPsolve(scip) );
+
+   SCIPinfoMessage(scip, NULL, "\nprimal solution:\n");
+   SCIPinfoMessage(scip, NULL, "================\n\n");
+   SCIP_CALL( SCIPprintBestSol(scip, NULL, FALSE) );
+
+
+   /**************
+    * Statistics *
+    **************/
+
+   /*
+   SCIPinfoMessage(scip, NULL, "\nStatistics\n");
+   SCIPinfoMessage(scip, NULL, "==========\n\n");
+
+   SCIP_CALL( SCIPprintStatistics(scip, NULL) );
+   */
+
+   return SCIP_OKAY;
+}
+
+/** evaluates command line parameters */
+static
+SCIP_RETCODE processShellArguments(
+   SCIP*                 scip,               /**< SCIP data structure */
+   int                   argc,               /**< number of shell parameters */
+   char**                argv,               /**< array with shell parameters */
+   const char*           defaultsetname      /**< name of default settings file */
+   )
+{  /*lint --e{850}*/
+   char* probname = NULL;
+   char* solname = NULL;
+   char* settingsname = NULL;
+   char* logname = NULL;
+   SCIP_Bool quiet;
+   SCIP_Bool paramerror;
+   int i;
+
+   /********************
+    * Parse parameters *
+    ********************/
+
+   quiet = FALSE;
+   paramerror = FALSE;
+   for( i = 1; i < argc; ++i )
+   {
+      if( strcmp(argv[i], "-l") == 0 )
+      {
+         i++;
+         if( i < argc )
+            logname = argv[i];
+         else
+         {
+            printf("missing log filename after parameter '-l'\n");
+            paramerror = TRUE;
+         }
+      }
+      else if( strcmp(argv[i], "-q") == 0 )
+         quiet = TRUE;
+      else if( strcmp(argv[i], "-s") == 0 )
+      {
+         i++;
+         if( i < argc )
+            settingsname = argv[i];
+         else
+         {
+            printf("missing settings filename after parameter '-s'\n");
+            paramerror = TRUE;
+         }
+      }
+      else if( strcmp(argv[i], "-f") == 0 )
+      {
+         i++;
+         if( i < argc )
+            probname = argv[i];
+         else
+         {
+            printf("missing problem filename after parameter '-f'\n");
+            paramerror = TRUE;
+         }
+      }
+      else if( strcmp(argv[i], "-o") == 0 )
+      {
+         i++;
+         if( i < argc )
+            solname = argv[i];
+         else
+         {
+            printf("missing optimal solution filename after parameter '-o'\n");
+            paramerror = TRUE;
+         }
+      }
+      else
+      {
+         printf("invalid parameter <%s>\n", argv[i]);
+         paramerror = TRUE;
+      }
+   }
+
+   if( !paramerror )
+   {
+      /***********************************
+       * create log file message handler *
+       ***********************************/
+
+      if( quiet )
+      {
+         SCIPsetMessagehdlrQuiet(scip, quiet);
+      }
+
+      if( logname != NULL )
+      {
+         SCIPsetMessagehdlrLogfile(scip, logname);
+      }
+
+      if ( solname != NULL )
+      {
+         SCIP_CALL( SCIPsetStringParam(scip, "nodeselection/oracle/solfname", solname) );
+      }
+         
+      SCIP_CALL( SCIPsetStringParam(scip, "vbc/filename", "test.vbc") );
+
+      /***********************************
+       * Version and library information *
+       ***********************************/
+
+      SCIPprintVersion(scip, NULL);
+      SCIPinfoMessage(scip, NULL, "\n");
+
+      SCIPprintExternalCodes(scip, NULL);
+      SCIPinfoMessage(scip, NULL, "\n");
+
+      /*****************
+       * Load settings *
+       *****************/
+
+      if( settingsname != NULL )
+      {
+         SCIP_CALL( SCIPreadParams(scip, settingsname) );
+      }
+      else if( defaultsetname != NULL )
+      {
+         SCIP_CALL( SCIPreadParams(scip, defaultsetname) );
+      }
+
+      /**************
+       * Start SCIP *
+       **************/
+
+      if( probname != NULL )
+      {
+         SCIP_CALL( fromCommandLine(scip, probname) );
+      }
+      else
+      {
+         return SCIP_OKAY;
+      }
+
+   }
+   else
+   {
+      printf("\nsyntax: %s [-l <logfile>] [-q] [-s <settings>] [-f <problem>] [-o <solution>]\n"
+         "  -l <logfile>  : copy output into log file\n"
+         "  -q            : suppress screen messages\n"
+         "  -s <settings> : load parameter settings (.set) file\n"
+         "  -f <problem>  : load and solve problem file\n"
+         "  -o <solution> : load optimal solution file\n",
+         argv[0]);
+   }
+
+   return SCIP_OKAY;
+}
+
+/** creates a SCIP instance with default plugins, evaluates command line parameters, runs SCIP appropriately,
+ *  and frees the SCIP instance
+ */
+static
+SCIP_RETCODE runShell(
+   int                        argc,               /**< number of shell parameters */
+   char**                     argv,               /**< array with shell parameters */
+   const char*                defaultsetname      /**< name of default settings file */
+   )
+{
+   SCIP* scip = NULL;
+   int num_heur = 0;
+   SCIP_HEUR** heurs = NULL;
+   SCIP_BRANCHRULE* branch_rule = NULL;
+   SCIP_NODESEL* nodesel = NULL;
+   int i;
+
+   /*********
+    * Setup *
+    *********/
+
+   /* initialize SCIP */
+   SCIP_CALL( SCIPcreate(&scip) );
+   
+   /* include default SCIP plugins */
+   SCIP_CALL( SCIPincludeDefaultPlugins(scip) );
+ 
+   /* include oracle node selector */
+   SCIP_CALL( SCIPincludeNodeselOracle(scip) );
+
+   /* disable heuristics */
+   num_heur = SCIPgetNHeurs( scip );
+   heurs = SCIPgetHeurs( scip );
+   for( i = 0; i < num_heur; i++ )
+   {
+      SCIPheurSetFreq( heurs[i], -1 );
+   }
+
+   /* use most infeasible branching */
+   branch_rule = SCIPfindBranchrule( scip, "mostinf" );
+   SCIP_CALL( SCIPsetBranchrulePriority( scip, branch_rule, 9999999 ) );
+   
+   /* use oracle node selector */
+   nodesel = SCIPfindNodesel( scip, "oracle" );
+   SCIP_CALL( SCIPsetNodeselStdPriority( scip, nodesel, 9999999 ) );
+   
+   /**********************************
+    * Process command line arguments *
+    **********************************/
+   SCIP_CALL( processShellArguments(scip, argc, argv, defaultsetname) );
+
+   /********************
+    * Deinitialization *
+    ********************/
+
+   SCIP_CALL( SCIPfree(&scip) );
+
+   BMScheckEmptyMemory();
+
+   return SCIP_OKAY;
+}
+
+int
+main(
+   int                        argc,
+   char**                     argv
+   )
+{
+   SCIP_RETCODE retcode;
+
+   retcode = runShell(argc, argv, NULL);
+   if( retcode != SCIP_OKAY )
+   {
+      SCIPprintError(retcode);
+      return -1;
+   }
+
+   return 0;
+}
+
---
+++ src/nodesel_oracle.c
@@ -0,0 +1,437 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2014 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**@file   nodesel_uct.c
+ * @brief  uct node selector which balances exploration and exploitation by considering node visits
+ * @author Gregor Hendel
+ *
+ * the UCT node selection rule selects the next leaf according to a mixed score of the node's actual lower bound
+ *
+ * The idea of UCT node selection for MIP appeared in:
+ *
+ * The authors adapted a game-tree exploration scheme called UCB to MIP trees. Starting from the root node as current node,
+ *
+ * The node selector features several parameters:
+ *
+ * @note It should be avoided to switch to uct node selection after the branch and bound process has begun because
+ */
+
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#include <assert.h>
+#include <string.h>
+#include "nodesel_oracle.h"
+#include "scip/sol.h"
+#include "scip/struct_scip.h"
+#include "scip/struct_set.h"
+
+#define NODESEL_NAME            "oracle"
+#define NODESEL_DESC            "node selector which always selects the optimal node"
+#define NODESEL_STDPRIORITY     10
+#define NODESEL_MEMSAVEPRIORITY 0
+
+#define DEFAULT_FILENAME        ""
+
+/*
+ * Data structures
+ */
+
+/** node selector data */
+struct SCIP_NodeselData
+{
+   SCIP_SOL*          optsol;             /**< optimal solution */
+   char*              solfname;           /**< name of the solution file */
+};
+
+/** check if the given node include the optimal solution */
+static
+SCIP_Bool nodeIsOptimal(
+   SCIP*                 scip,               /**< SCIP data structure */
+   SCIP_NODESELDATA*     nodeseldata,        /**< node selector data */
+   SCIP_NODE*            node                /**< the node in question */
+   )
+{
+   /* get parent branch vars lead to this node */
+
+   SCIP_VAR**            branchvars;         /* array of variables on which the branchings has been performed in all ancestors */
+   SCIP_Real*            branchbounds;       /* array of bounds which the branchings in all ancestors set */
+   SCIP_BOUNDTYPE*       boundtypes;         /* array of boundtypes which the branchings in all ancestors set */
+   int                   nbranchvars;        /* number of variables on which branchings have been performed in all ancestors
+                                              *   if this is larger than the array size, arrays should be reallocated and method should be called again */
+   int                   branchvarssize;     /* available slots in arrays */
+
+   int i;
+
+   assert(node != NULL);
+
+   branchvarssize = 1; 
+
+   /* memory allocation */
+   SCIP_CALL( SCIPallocBufferArray(scip, &branchvars, branchvarssize) );
+   SCIP_CALL( SCIPallocBufferArray(scip, &branchbounds, branchvarssize) );
+   SCIP_CALL( SCIPallocBufferArray(scip, &boundtypes, branchvarssize) );
+
+   SCIPnodeGetParentBranchings(node, branchvars, branchbounds, boundtypes, &nbranchvars, branchvarssize);
+
+   /* if the arrays were to small, we have to reallocate them and recall SCIPnodeGetParentBranchings */
+   if( nbranchvars > branchvarssize )
+   {
+      branchvarssize = nbranchvars;
+
+      /* memory reallocation */
+      SCIP_CALL( SCIPreallocBufferArray(scip, &branchvars, branchvarssize) );
+      SCIP_CALL( SCIPreallocBufferArray(scip, &branchbounds, branchvarssize) );
+      SCIP_CALL( SCIPreallocBufferArray(scip, &boundtypes, branchvarssize) );
+      
+      SCIPnodeGetParentBranchings(node, branchvars, branchbounds, boundtypes, &nbranchvars, branchvarssize);
+      assert(nbranchvars == branchvarssize);
+   }
+
+   /* check optimality */
+   assert(nbranchvars >= 1);
+   for( i = 0; i < nbranchvars; ++i)
+   {
+      SCIP_Real optval = SCIPgetSolVal(scip, nodeseldata->optsol, branchvars[i]);
+      if( (boundtypes[i] == SCIP_BOUNDTYPE_LOWER && optval < branchbounds[i]) ||
+          (boundtypes[i] == SCIP_BOUNDTYPE_UPPER && optval > branchbounds[i]) )
+         return FALSE;
+   }
+   return TRUE;
+}
+
+/** read the optimal solution (modified from readSol in reader_sol.c -- don't connect the solution with primal solutions) */
+/** TODO: currently the read objective is wrong, since it doesn not include objetives from multi-aggregated variables */
+static
+SCIP_RETCODE readOptSol(
+   SCIP*                 scip,               /**< SCIP data structure */
+   const char*           fname,              /**< name of the input file */
+   SCIP_SOL**            sol                 /**< pointer to store the solution */
+   )
+{
+   SCIP_FILE* file;
+   SCIP_Bool error;
+   SCIP_Bool unknownvariablemessage;
+   SCIP_Bool usevartable;
+   int lineno;
+
+   printf("Read solution at stage %d\n", scip->set->stage);
+
+   assert(scip != NULL);
+   assert(fname != NULL);
+
+   SCIP_CALL( SCIPgetBoolParam(scip, "misc/usevartable", &usevartable) );
+
+   if( !usevartable )
+   {
+      SCIPerrorMessage("Cannot read solution file if vartable is disabled. Make sure parameter 'misc/usevartable' is set to TRUE.\n");
+      return SCIP_READERROR;
+   }
+
+   /* open input file */
+   file = SCIPfopen(fname, "r");
+   if( file == NULL )
+   {
+      SCIPerrorMessage("cannot open file <%s> for reading\n", fname);
+      SCIPprintSysError(fname);
+      return SCIP_NOFILE;
+   }
+
+   /* create zero solution */
+   SCIP_CALL( SCIPcreateSolSelf(scip, sol, NULL) );
+   assert(SCIPsolIsOriginal(*sol) == TRUE);
+   
+   /* read the file */
+   error = FALSE;
+   unknownvariablemessage = FALSE;
+   lineno = 0;
+   while( !SCIPfeof(file) && !error )
+   {
+      char buffer[SCIP_MAXSTRLEN];
+      char varname[SCIP_MAXSTRLEN];
+      char valuestring[SCIP_MAXSTRLEN];
+      char objstring[SCIP_MAXSTRLEN];
+      SCIP_VAR* var;
+      SCIP_Real value;
+      int nread;
+
+      /* get next line */
+      if( SCIPfgets(buffer, (int) sizeof(buffer), file) == NULL )
+         break;
+      lineno++;
+
+      /* there are some lines which may preceed the solution information */
+      if( strncasecmp(buffer, "solution status:", 16) == 0 || strncasecmp(buffer, "objective value:", 16) == 0 ||
+         strncasecmp(buffer, "Log started", 11) == 0 || strncasecmp(buffer, "Variable Name", 13) == 0 ||
+         strncasecmp(buffer, "All other variables", 19) == 0 || strncasecmp(buffer, "\n", 1) == 0 || 
+         strncasecmp(buffer, "NAME", 4) == 0 || strncasecmp(buffer, "ENDATA", 6) == 0 )    /* allow parsing of SOL-format on the MIPLIB 2003 pages */
+         continue;
+
+      /* parse the line */
+      nread = sscanf(buffer, "%s %s %s\n", varname, valuestring, objstring);
+      if( nread < 2 )
+      {
+         SCIPerrorMessage("Invalid input line %d in solution file <%s>: <%s>.\n", lineno, fname, buffer);
+         error = TRUE;
+         break;
+      }
+
+      /* find the variable */
+      var = SCIPfindVar(scip, varname);
+      if( var == NULL )
+      {
+         if( !unknownvariablemessage )
+         {
+            SCIPverbMessage(scip, SCIP_VERBLEVEL_NORMAL, NULL, "unknown variable <%s> in line %d of solution file <%s>\n", 
+               varname, lineno, fname);
+            SCIPverbMessage(scip, SCIP_VERBLEVEL_NORMAL, NULL, "  (further unknown variables are ignored)\n");
+            unknownvariablemessage = TRUE;
+         }
+         continue;
+      }
+
+      /* cast the value */
+      if( strncasecmp(valuestring, "inv", 3) == 0 )
+         continue;
+      else if( strncasecmp(valuestring, "+inf", 4) == 0 || strncasecmp(valuestring, "inf", 3) == 0 )
+         value = SCIPinfinity(scip);
+      else if( strncasecmp(valuestring, "-inf", 4) == 0 )
+         value = -SCIPinfinity(scip);
+      else
+      {
+         nread = sscanf(valuestring, "%lf", &value);
+         if( nread != 1 )
+         {
+            SCIPerrorMessage("Invalid solution value <%s> for variable <%s> in line %d of solution file <%s>.\n",
+               valuestring, varname, lineno, fname);
+            error = TRUE;
+            break;
+         }
+      }
+
+      /* set the solution value of the variable, if not multiaggregated */
+      if( SCIPisTransformed(scip) && SCIPvarGetStatus(SCIPvarGetProbvar(var)) == SCIP_VARSTATUS_MULTAGGR )
+      {
+         SCIPverbMessage(scip, SCIP_VERBLEVEL_NORMAL, NULL, "ignored solution value for multiaggregated variable <%s>\n", SCIPvarGetName(var));
+      }
+      else
+      {
+         SCIP_RETCODE retcode;
+         retcode =  SCIPsetSolVal(scip, *sol, var, value);
+
+         if( retcode == SCIP_INVALIDDATA )
+         {
+            if( SCIPvarGetStatus(SCIPvarGetProbvar(var)) == SCIP_VARSTATUS_FIXED )
+            {
+               SCIPverbMessage(scip, SCIP_VERBLEVEL_NORMAL, NULL, "ignored conflicting solution value for fixed variable <%s>\n",
+                  SCIPvarGetName(var));
+            }
+            else
+            {
+               SCIPverbMessage(scip, SCIP_VERBLEVEL_NORMAL, NULL, "ignored solution value for multiaggregated variable <%s>\n",
+                  SCIPvarGetName(var));
+            }
+         }
+         else
+         {
+            SCIP_CALL( retcode );
+         }
+      }
+   }
+
+   /* close input file */
+   SCIPfclose(file);
+
+   if( !error )
+   {
+      assert(SCIPgetNSols(scip) == 0);
+      /* display result */
+      SCIPverbMessage(scip, SCIP_VERBLEVEL_NORMAL, NULL, "optimal solution from solution file <%s> was %s\n",
+         fname, "read, will be used in the oracle node selector");
+      return SCIP_OKAY;
+   }
+   else
+   {
+      /* free solution */
+      SCIP_CALL( SCIPfreeSolSelf(scip, sol) );
+
+      return SCIP_READERROR;
+   }
+}
+
+/*
+ * Callback methods of node selector
+ */
+
+/** copy method for node selector plugins (called when SCIP copies plugins) */
+static
+SCIP_DECL_NODESELCOPY(nodeselCopyOracle)
+{  /*lint --e{715}*/
+   assert(scip != NULL);
+   SCIP_CALL( SCIPincludeNodeselOracle(scip) );
+
+   return SCIP_OKAY;
+}
+
+/** solving process initialization method of node selector (called when branch and bound process is about to begin) */
+static
+SCIP_DECL_NODESELINITSOL(nodeselInitsolOracle)
+{
+   SCIP_NODESELDATA* nodeseldata;
+   assert(scip != NULL);
+   assert(nodesel != NULL);
+
+   nodeseldata = SCIPnodeselGetData(nodesel);
+
+   assert(nodeseldata != NULL);
+   /** solfname should be set before including nodeseloracle */
+   assert(nodeseldata->solfname != NULL);
+   nodeseldata->optsol = NULL;
+
+   readOptSol(scip, nodeseldata->solfname, &nodeseldata->optsol);
+   assert(nodeseldata->optsol != NULL);
+  
+   SCIP_CALL( SCIPprintSol(scip, nodeseldata->optsol, NULL, FALSE) ); 
+
+   return SCIP_OKAY;
+}
+
+/** destructor of node selector to free user data (called when SCIP is exiting) */
+static
+SCIP_DECL_NODESELFREE(nodeselFreeOracle)
+{
+   SCIP_NODESELDATA* nodeseldata;
+   assert(scip != NULL);
+   assert(nodesel != NULL);
+
+   nodeseldata = SCIPnodeselGetData(nodesel);
+
+   assert(nodeseldata->optsol != NULL);
+   SCIP_CALL( SCIPfreeSolSelf(scip, &nodeseldata->optsol) );
+
+   SCIPfreeBlockMemory(scip, &nodeseldata);
+
+   SCIPnodeselSetData(nodesel, NULL);
+
+   return SCIP_OKAY;
+}
+
+/** node selection method of node selector */
+static
+SCIP_DECL_NODESELSELECT(nodeselSelectOracle)
+{
+   assert(nodesel != NULL);
+   assert(strcmp(SCIPnodeselGetName(nodesel), NODESEL_NAME) == 0);
+   assert(scip != NULL);
+   assert(selnode != NULL);
+
+   *selnode = SCIPgetBestNode(scip);
+
+   return SCIP_OKAY;
+}
+
+/** node comparison method of oracle node selector */
+static
+SCIP_DECL_NODESELCOMP(nodeselCompOracle)
+{  /*lint --e{715}*/
+   SCIP_Bool isoptimal1;
+   SCIP_Bool isoptimal2;
+   SCIP_NODESELDATA* nodeseldata;
+
+   assert(nodesel != NULL);
+   assert(strcmp(SCIPnodeselGetName(nodesel), NODESEL_NAME) == 0);
+   assert(scip != NULL);
+
+   nodeseldata = SCIPnodeselGetData(nodesel);
+   assert(nodeseldata != NULL);
+   assert(nodeseldata->optsol != NULL);
+
+   isoptimal1 = nodeIsOptimal(scip, nodeseldata, node1);
+   isoptimal2 = nodeIsOptimal(scip, nodeseldata, node2);
+
+   if( isoptimal1 == TRUE )
+   {
+      assert(isoptimal2 != TRUE);
+      return -1;
+   }
+   else if( isoptimal2 == TRUE )
+      return +1;
+   else
+   {
+      int depth1;
+      int depth2;
+
+      depth1 = SCIPnodeGetDepth(node1);
+      depth2 = SCIPnodeGetDepth(node2);
+      if( depth1 > depth2 )
+         return -1;
+      else if( depth1 < depth2 )
+         return +1;
+      else
+      {
+         SCIP_Real lowerbound1;
+         SCIP_Real lowerbound2;
+
+         lowerbound1 = SCIPnodeGetLowerbound(node1);
+         lowerbound2 = SCIPnodeGetLowerbound(node2);
+         if( lowerbound1 < lowerbound2 )
+            return -1;
+         else if( lowerbound1 > lowerbound2 )
+            return +1;
+         else
+            return 0;
+      }
+   }
+}
+
+/*
+ * node selector specific interface methods
+ */
+
+/** creates the uct node selector and includes it in SCIP */
+SCIP_RETCODE SCIPincludeNodeselOracle(
+   SCIP*                 scip                /**< SCIP data structure */
+   )
+{
+   SCIP_NODESELDATA* nodeseldata;
+   SCIP_NODESEL* nodesel;
+
+   /* create oracle node selector data */
+   SCIP_CALL( SCIPallocBlockMemory(scip, &nodeseldata) );
+
+   nodesel = NULL;
+   nodeseldata->optsol = NULL;
+   nodeseldata->solfname = NULL;
+
+   /* use SCIPincludeNodeselBasic() plus setter functions if you want to set callbacks one-by-one and your code should
+    * compile independent of new callbacks being added in future SCIP versions
+    */
+   SCIP_CALL( SCIPincludeNodeselBasic(scip, &nodesel, NODESEL_NAME, NODESEL_DESC, NODESEL_STDPRIORITY,
+          NODESEL_MEMSAVEPRIORITY, nodeselSelectOracle, nodeselCompOracle, nodeseldata) );
+
+   assert(nodesel != NULL);
+
+   /* set non fundamental callbacks via setter functions */
+   SCIP_CALL( SCIPsetNodeselCopy(scip, nodesel, nodeselCopyOracle) );
+   SCIP_CALL( SCIPsetNodeselInitsol(scip, nodesel, nodeselInitsolOracle) );
+   SCIP_CALL( SCIPsetNodeselFree(scip, nodesel, nodeselFreeOracle) );
+
+   /* add oracle node selector parameters */
+   SCIP_CALL( SCIPaddStringParam(scip, 
+         "nodeselection/"NODESEL_NAME"/solfname",
+         "name of the optimal solution file",
+         &nodeseldata->solfname, FALSE, DEFAULT_FILENAME, NULL, NULL) );
+
+   return SCIP_OKAY;
+}
